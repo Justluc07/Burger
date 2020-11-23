43f231de5c56ee794084126aaa1b24eb203a562f
Justin Lucas <justluc07@gmail.com>
2020-11-22 22:39:31 -0500

    upload

---
+++ Unsolved/db/seeds.sql
@@ -0,0 +1,6 @@
+INSERT INTO Burgers (name) VALUES ('Molly');
+INSERT INTO Burgers (name) VALUES ('Charlie');
+INSERT INTO Burgers (name, devoured) VALUES ('Poppy', true);
+INSERT INTO Burgers (name, devoured) VALUES ('Oscar', true);
+INSERT INTO Burgers (name, devoured) VALUES ('Smudge', true);
+INSERT INTO Burgers (name) VALUES ('Daisy');
